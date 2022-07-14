c60f28e4da84a3c4d622720e0d8588724f89bbd7
Stephen Thirlwall <stephent@logicly.com.au>
2022-07-14 13:25:55 +1000

    Don't decorate errors for the header row
    It makes sense for data rows, but not the header row

--- lib/csvlint/cli.rb
+++ lib/csvlint/cli.rb
@@ -176,7 +176,7 @@ module Csvlint
         col: error.column
       }
 
-      if error.column && @schema && @schema.instance_of?(Csvlint::Schema) && @schema.fields[error.column - 1] != nil
+      if error.column && @schema && @schema.instance_of?(Csvlint::Schema) && @schema.fields[error.column - 1] != nil && error.row != 1
         field = @schema.fields[error.column - 1]
         h[:header] = field.name
         h[:constraints] = field.constraints.map { |k, v| [k.underscore, v] }.to_h

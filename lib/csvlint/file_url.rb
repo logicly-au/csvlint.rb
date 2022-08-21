23d7e79bccb77cd7a045c0e13cabbc32d48c3fb8
Russell Jenkins <russell.jenkins@strategicdata.com.au>
2022-08-21 11:32:40 +1000

    Update FileUrl for Ruby3

--- lib/csvlint/file_url.rb
+++ lib/csvlint/file_url.rb
@@ -2,13 +2,13 @@ module Csvlint
   module FileUrl
     # Convert a path to an absolute file:// uri
     def self.url(path)
-      URI.encode(File.expand_path(path).gsub(/^\/*/, "file:///"))
+      URI.encode_www_form_component(File.expand_path(path).gsub(/^\/*/, "file:///"))
     end
 
     # Convert an file:// uri to a File
     def self.file(uri)
-      if uri.start_with?("file:")
-        uri = URI.decode(uri)
+      if /^file:/.match?(uri.to_s)
+        uri = URI.decode_www_form_component(uri)
         uri = uri.gsub(/^file:\/*/, "/")
         File.new(uri)
       else
